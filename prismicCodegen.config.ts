d62173e6a15e2bb9471d5c515d98de1c62640094
timothe <timothe@rezo-zero.com>
2023-04-26 01:54:25 +0200

    chore(config): add information about ts-code-gen

--- prismicCodegen.config.ts
+++ prismicCodegen.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "prismic-ts-codegen";
 const config: Config = {
     output: "./src/types/prismic/prismic-types.generated.ts",
     models: ["./customtypes/**/index.json", "./slices/**/model.json"],
+
+    // Loading remote models with the Custom Types API
+    // https://prismic.io/docs/technical-reference/prismic-ts-codegen
     repositoryName: process.env.PRISMIC_REPOSITORY_NAME,
     customTypesAPIToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
     clientIntegration: {
